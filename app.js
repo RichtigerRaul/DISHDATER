7d97631e750e8324110ff35770d0badf6dffda06
Raul <ibragraul@gmail.com>
2024-10-05 17:23:07 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
+// app.js
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Lade die Einstellungen, falls auf der Einstellungen-Seite
+    if (document.getElementById('animal-checkbox') && document.getElementById('milk-checkbox')) {
+        loadSettings();
+    }
+    
+    // Eventlistener für "Zurück" Button in den Einstellungen
+    if (document.getElementById('back-button')) {
+        document.getElementById('back-button').addEventListener('click', function() {
+            saveSettings();
+            goBack();
+        });
+    }
+});
 
