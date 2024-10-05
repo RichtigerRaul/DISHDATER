3c356cdbd333fa5a9c6961f9ecb88835381f1b12
Raul <ibragraul@gmail.com>
2024-10-05 17:27:03 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 // app.js
 
 document.addEventListener("DOMContentLoaded", function() {
-    // Lade die Einstellungen, falls auf der Einstellungen-Seite
+    // Start-Button-Logik für index.html
+    const startButton = document.getElementById('start-button');
+    
+    if (startButton) {
+        startButton.addEventListener('click', function() {
+            navigateTo('menu.html');
+        });
+    }
+
+    // Einstellungen, wenn auf settings.html
     if (document.getElementById('animal-checkbox') && document.getElementById('milk-checkbox')) {
         loadSettings();
     }
-    
+
     // Eventlistener für "Zurück" Button in den Einstellungen
     if (document.getElementById('back-button')) {
         document.getElementById('back-button').addEventListener('click', function() {
@@ -14,4 +23,3 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 });
-
