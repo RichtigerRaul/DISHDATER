63278ad3ab0d0d8081faaeb7c6b5a9b75d2fff2c
Raul <ibragraul@gmail.com>
2024-10-05 17:22:17 +0200

    Update navigation.js

--- navigation.js
+++ navigation.js
@@ -1,1 +1,10 @@
+// navigation.js
+
+function navigateTo(page) {
+    window.location.href = page; // Weiterleitung zur gewählten Seite
+}
+
+function goBack() {
+    window.history.back(); // Gehe zur vorherigen Seite zurück
+}
 
