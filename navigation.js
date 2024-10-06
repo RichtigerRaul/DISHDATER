251329086a375291180bb4a3a4fd32153bdad44c
Raul <ibragraul@gmail.com>
2024-10-06 02:00:47 +0200

    Delete navigation.js

--- navigation.js
+++
@@ -1,6 +0,0 @@
-// navigation.js
-
-// Funktion für die Navigation zu einer neuen Seite
-function navigateTo(page) {
-    window.location.href = page; // Weiterleitung zur gewählten Seite (z.B. menu.html)
-}
