9ad8c4fa6688fa5311f1ac8d7e31c8c76a1aca34
Raul <ibragraul@gmail.com>
2024-10-06 02:00:37 +0200

    Update navigation.js

--- js/navigation.js
+++ js/navigation.js
@@ -1,1 +1,6 @@
+// navigation.js
 
+// Funktion für die Navigation zu einer neuen Seite
+function navigateTo(page) {
+    window.location.href = page; // Weiterleitung zur gewählten Seite (z.B. menu.html)
+}
