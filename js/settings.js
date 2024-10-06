5a572e50f274b9645f139f4dc05e8ded5e3396da
Raul <ibragraul@gmail.com>
2024-10-06 02:01:12 +0200

    Update settings.js

--- js/settings.js
+++ js/settings.js
@@ -1,1 +1,16 @@
+// settings.js
 
+// Lade die gespeicherten Einstellungen
+function loadSettings() {
+    const animalPref = localStorage.getItem('animal') === 'true';
+    const milkPref = localStorage.getItem('milk') === 'true';
+
+    document.getElementById('animal-checkbox').checked = animalPref;
+    document.getElementById('milk-checkbox').checked = milkPref;
+}
+
+// Speichere die Einstellungen
+function saveSettings() {
+    localStorage.setItem('animal', document.getElementById('animal-checkbox').checked);
+    localStorage.setItem('milk', document.getElementById('milk-checkbox').checked);
+}
