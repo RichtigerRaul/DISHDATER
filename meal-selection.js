ed6762ef0067ff473ec875de74e34fcd7541531d
Raul <ibragraul@gmail.com>
2024-10-11 18:27:40 +0200

    Update meal-selection.js

--- meal-selection.js
+++ meal-selection.js
@@ -1,10 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const buttons = document.querySelectorAll('button');
-    buttons.forEach(button => {
-        button.addEventListener('click', () => {
-            const meal = button.id;
-            localStorage.setItem('selectedMeal', meal);
-            window.location.href = 'dishdater.html';
-        });
-    });
+    const breakfastButton = document.getElementById('breakfastButton');
+    const lunchButton = document.getElementById('lunchButton');
+    const dinnerButton = document.getElementById('dinnerButton');
+
+    breakfastButton.addEventListener('click', () => selectMeal('Frühstück'));
+    lunchButton.addEventListener('click', () => selectMeal('Mittagessen'));
+    dinnerButton.addEventListener('click', () => selectMeal('Abendessen'));
 });
+
+function selectMeal(meal) {
+    localStorage.setItem('selectedMeal', meal);
+    window.location.href = 'dishdater.html';
+}
